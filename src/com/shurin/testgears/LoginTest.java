73983d2dc50474b1871172a66b1670b2e0ad8542
Shurin <ThaVisual@hotmail.com>
2014-03-23 03:23:12 +0100

    Changes

--- src/com/shurin/testgears/LoginTest.java
+++ src/com/shurin/testgears/LoginTest.java
@@ -7,10 +7,15 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.SystemColor;
 
 public class LoginTest extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -31,11 +36,33 @@ public class LoginTest extends JDialog {
 	public LoginTest() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBackground(SystemColor.activeCaption);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblUsername = new JLabel("Username");
+			lblUsername.setBounds(173, 71, 74, 24);
+			contentPanel.add(lblUsername);
+		}
+		{
+			JLabel lblPassword = new JLabel("Password");
+			lblPassword.setBounds(173, 102, 74, 24);
+			contentPanel.add(lblPassword);
+		}
+		
+		textField = new JTextField();
+		textField.setBounds(257, 73, 86, 20);
+		contentPanel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(257, 104, 86, 20);
+		contentPanel.add(textField_1);
 		{
 			JPanel buttonPane = new JPanel();
+			buttonPane.setBackground(SystemColor.activeCaption);
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
@@ -51,5 +78,4 @@ public class LoginTest extends JDialog {
 			}
 		}
 	}
-
 }
