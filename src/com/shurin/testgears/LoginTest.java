d0f4212bfae37d876ab30ab211b4acc6bbd69c65
Shurin <ThaVisual@hotmail.com>
2014-03-23 23:36:48 +0100

    changes

--- src/com/shurin/testgears/LoginTest.java
+++ src/com/shurin/testgears/LoginTest.java
@@ -2,15 +2,20 @@ package com.shurin.testgears;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.awt.SystemColor;
-import javax.swing.ImageIcon;
+import javax.swing.border.EmptyBorder;
 
 public class LoginTest extends JDialog {
 
@@ -35,12 +40,14 @@ public class LoginTest extends JDialog {
 	 * Create the dialog.
 	 */
 	public LoginTest() {
+		setTitle("G.I.R.S.");
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBackground(SystemColor.activeCaption);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
+		setLocationRelativeTo(null);
 		{
 			JLabel lblUsername = new JLabel("Username");
 			lblUsername.setBounds(115, 144, 74, 24);
@@ -51,19 +58,20 @@ public class LoginTest extends JDialog {
 			lblPassword.setBounds(115, 175, 74, 24);
 			contentPanel.add(lblPassword);
 		}
-		
+
 		textField = new JTextField();
 		textField.setBounds(199, 146, 86, 20);
 		contentPanel.add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
 		textField_1.setBounds(199, 177, 86, 20);
 		contentPanel.add(textField_1);
-		
+
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(LoginTest.class.getResource("/com/shurin/testgears/TestLogoIcon.jpg")));
+		lblNewLabel.setIcon(new ImageIcon(LoginTest.class
+				.getResource("/com/shurin/testgears/TestLogoIcon.jpg")));
 		lblNewLabel.setBounds(55, 11, 317, 131);
 		contentPanel.add(lblNewLabel);
 		{
@@ -75,13 +83,26 @@ public class LoginTest extends JDialog {
 				JButton okButton = new JButton("OK");
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						setVisible(false);
+						MainScreen.main(null);
+					}
+				});
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
 				cancelButton.setActionCommand("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						System.out.println("Cancel");
+						// What action do you want to add in each button?
+					}
+				});
 				buttonPane.add(cancelButton);
 			}
 		}
+
 	}
 }
--- src/com/shurin/testgears/MainScreen.java
+++ src/com/shurin/testgears/MainScreen.java
@@ -1,19 +1,22 @@
 package com.shurin.testgears;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JMenu;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
 
 public class MainScreen extends JFrame {
 
-	private JPanel contentPane;
+	private JScrollPane contentPane;
 
 	/**
 	 * Launch the application.
@@ -35,12 +38,49 @@ public class MainScreen extends JFrame {
 	 * Create the frame.
 	 */
 	public MainScreen() {
+		setTitle("G.I.R.S.");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 405);
-		contentPane = new JPanel();
-		contentPane.setBackground(SystemColor.activeCaption);
+		setResizable(false);
+		contentPane = new JScrollPane();
+		Color myColor = new Color(10, 10, 10, 10);
+		contentPane.setBackground(myColor);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		setLocationRelativeTo(null);
+
+		JMenuBar bar = new JMenuBar();
+		setJMenuBar(bar);
+
+		JMenu file = new JMenu("File");
+		file.setMnemonic('F');
+		JMenu help = new JMenu("Help");
+		help.setMnemonic('H');
+
+		bar.add(file);
+		bar.add(help);
+
+		JMenuItem login = new JMenuItem("Open Login");
+		login.setMnemonic('L');
+		login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				LoginTest.main(null);
+			}
+		});
+
+		file.add(login);
+
+		JButton okButton = new JButton("OK");
+		okButton.setActionCommand("OK");
+		okButton.setBounds(50, 350, 50, 50);
+		contentPane.add(okButton);
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				MainScreen.main(null);
+			}
+		});
 	}
 }
