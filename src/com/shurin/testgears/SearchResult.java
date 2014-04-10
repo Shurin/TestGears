976072322819efac5267b2332195012eba34fdd6
joaogl <joaoguerralourenco@hotmail.com>
2014-04-10 18:10:33 +0100

    Removed Warnings.

--- src/com/shurin/testgears/SearchResult.java
+++ src/com/shurin/testgears/SearchResult.java
@@ -2,23 +2,23 @@ package com.shurin.testgears;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.SystemColor;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import java.awt.Font;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 
 public class SearchResult extends JDialog {
-
+	private static final long serialVersionUID = 1L;
+	
 	private final JPanel contentPanel = new JPanel();
 	private JTextField txtFbsbv;
 
