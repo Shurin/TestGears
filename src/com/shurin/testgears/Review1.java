70c1d173b4a9419b7fa4559aeee383cc9553515f
joaogl <joaoguerralourenco@hotmail.com>
2014-04-10 18:41:57 +0100

    Code cleanup.

--- src/com/shurin/testgears/Review1.java
+++ src/com/shurin/testgears/Review1.java
@@ -10,9 +10,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -21,21 +18,9 @@ import javax.swing.UIManager;
 
 public class Review1 extends JFrame {
 	private static final long serialVersionUID = 1L;
-	
+
 	private JPanel contentPane;
-	private JTextField textFieldFirstName;
-	private JTextField textFieldLastName;
-	private JTextField textFieldAddress;
-	private JTextField textFieldPoB;
-	private JTextField textFieldHairColor;
-	private JTextField textFieldEyeColor;
-	private JTextField txtJosm;
-	private JTextField textFieldMonth;
-	private JTextField textFieldDay;
-	private JTextField textFieldYear;
-	private JTextField textFieldBloodType;
-	private JTextField textFieldDepartmantA;
-	private JTextField textFieldDepartmantB, textFieldDapartmentC, textFieldDepartmentD;
+	private JTextField textFieldFirstName, textFieldLastName, textFieldAddress, textFieldPoB, textFieldHairColor, textFieldEyeColor, txtJosm, textFieldMonth, textFieldDay, textFieldYear, textFieldBloodType, textFieldDepartmantA, textFieldDepartmantB, textFieldDapartmentC, textFieldDepartmentD;
 	private JButton btnAccept;
 
 	/**
@@ -336,73 +321,7 @@ public class Review1 extends JFrame {
 		});
 		contentPane.add(btnAccept);
 
-		JMenuBar bar = new JMenuBar();
-		bar.setBorder(UIManager.getBorder("DesktopIcon.border"));
-		setJMenuBar(bar);
-
-		JMenu file = new JMenu("File");
-		file.setMnemonic('F');
-		JMenu help = new JMenu("Search");
-		help.setMnemonic('S');
-
-		bar.add(file);
-		bar.add(help);
-
-		JMenuItem mntmSearchEntries = new JMenuItem("Search Entries");
-		mntmSearchEntries.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				Search.main(null);
-			}
-		});
-		help.add(mntmSearchEntries);
-
-		JMenuItem login = new JMenuItem("Back to Login");
-		login.setMnemonic('L');
-		login.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				LoginTest.main(null);
-			}
-		});
-
-		file.add(login);
-
-		JMenuItem mntmOpenAddinfo = new JMenuItem("Add New Person");
-		mntmOpenAddinfo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				setVisible(false);
-				AddInfo.main(null);
-			}
-		});
-		file.add(mntmOpenAddinfo);
-
-		JMenuItem mntmOpenProductregistration = new JMenuItem("Add New Product");
-		mntmOpenProductregistration.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				ProductRegistration.main(null);
-			}
-		});
-
-		JMenuItem mntmReviewNewPerson = new JMenuItem("Review New Person");
-		mntmReviewNewPerson.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				setVisible(false);
-				Review1.main(null);
-			}
-		});
-		file.add(mntmReviewNewPerson);
-		file.add(mntmOpenProductregistration);
-
-		JMenuItem mntmReviewNewProduct = new JMenuItem("Review New Product");
-		mntmReviewNewProduct.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				Review2.main(null);
-			}
-		});
-		file.add(mntmReviewNewProduct);
+		new TopMenu(this);
 
 	}
 }
