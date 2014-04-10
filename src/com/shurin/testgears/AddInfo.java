32e83ddc8d70b804068181e1988bfe5e87652cdd
joaogl <joaoguerralourenco@hotmail.com>
2014-04-10 18:30:14 +0100

    Reset button now resets all the fields.

--- src/com/shurin/testgears/AddInfo.java
+++ src/com/shurin/testgears/AddInfo.java
@@ -32,6 +32,8 @@ public class AddInfo extends JFrame {
 	private JTextField textFieldHairColor;
 	private JTextField textFieldEyeColor;
 	private JTextField txtJosm;
+	@SuppressWarnings("rawtypes")
+	private JComboBox comboBoxMonth, comboBoxDay, comboBoxYear, comboBox;
 
 	/**
 	 * Launch the application.
@@ -153,26 +155,26 @@ public class AddInfo extends JFrame {
 		lblCIN.setBounds(487, 180, 33, 14);
 		contentPane.add(lblCIN);
 
-		JComboBox comboBoxMonth = new JComboBox();
+		comboBoxMonth = new JComboBox();
 		comboBoxMonth.setFont(new Font("Eras Demi ITC", Font.PLAIN, 12));
 		comboBoxMonth.setModel(new DefaultComboBoxModel(new String[] { "Month", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" }));
 		comboBoxMonth.setBounds(197, 278, 63, 20);
 		contentPane.add(comboBoxMonth);
 
-		JComboBox comboBoxDay = new JComboBox();
+		comboBoxDay = new JComboBox();
 		comboBoxDay.setModel(new DefaultComboBoxModel(new String[] { "Day", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" }));
 		comboBoxDay.setFont(new Font("Eras Demi ITC", Font.PLAIN, 12));
 		comboBoxDay.setBounds(270, 278, 47, 20);
 		contentPane.add(comboBoxDay);
 
-		JComboBox comboBoxYear = new JComboBox();
+		comboBoxYear = new JComboBox();
 		comboBoxYear.setModel(new DefaultComboBoxModel(new String[] { "Year", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000", "1999", "1998", "1997", "1996", "1995", "1994", "1993", "1992", "1991", "1990", "1989", "1988", "1987", "1986", "1985", "1984", "1983", "1982", "1981", "1980", "1979", "1978", "1977", "1976", "1975", "1974", "1973", "1972", "1971", "1970", "1969", "1968", "1967", "1966", "1965", "1964", "1963", "1962", "1961", "1960", "1959", "1958", "1957", "1956", "1955", "1954", "1953", "1952", "1951", "1950", "1949", "1948", "1947", "1946", "1945", "1944", "1943", "1942", "1941", "1940", "1939", "1938", "1937", "1936", "1935", "1934", "1933", "1932", "1931", "1930", "1929", "1928", "1927", "1926", "1925", "1924", "1923", "1922", "1921", "1920", "1919", "1918", "1917", "1916", "1915", "1914", "1913", "1912", "1911", "1910", "1909", "1908", "1907", "1906", "1905", "1904", "1903", "1902", "1901",
 				"1900" }));
 		comboBoxYear.setFont(new Font("Eras Demi ITC", Font.PLAIN, 12));
 		comboBoxYear.setBounds(327, 278, 53, 20);
 		contentPane.add(comboBoxYear);
 
-		JComboBox comboBox = new JComboBox();
+		comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] { "O-", "O+", "A-", "A+", "B-", "B+", "AB-", "AB+" }));
 		comboBox.setFont(new Font("Eras Demi ITC", Font.PLAIN, 12));
 		comboBox.setBounds(563, 345, 49, 20);
@@ -182,6 +184,15 @@ public class AddInfo extends JFrame {
 		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				textFieldFirstName.setText("");
+				textFieldLastName.setText("");
+				textFieldAddress.setText("");
+				textFieldPoB.setText("");
+				textFieldHairColor.setText("");
+				textFieldEyeColor.setText("");
+				comboBoxMonth.setSelectedIndex(0);
+				comboBoxDay.setSelectedIndex(0);
+				comboBoxYear.setSelectedIndex(0);
+				comboBox.setSelectedIndex(0);
 			}
 		});
 		btnCancel.setFont(new Font("Eras Demi ITC", Font.PLAIN, 12));
